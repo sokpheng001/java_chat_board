9316b67c47f8baa6bbb3622f93414b8b7fd7fad3
sokpheng001 <kimchansokpheng123@gmail.com>
2025-02-27 11:20:28 +0700

    add: added documentation comments for classes for make sense

--- src/main/java/bean/UserBean.java
+++ src/main/java/bean/UserBean.java
@@ -5,6 +5,11 @@ import mapper.UserManualMapper;
 import repository.UserRepositoryImpl;
 import repository.abstraction.UserRepository;
 
+/**
+ * <h>This class is created for manage the User class's object</h>
+ * @author kimchansokpheng
+ * @version 1
+ */
 public class UserBean {
     public static UserController userController = new UserController();
     public static UserRepository userRepository = new UserRepositoryImpl();
