bc532f357012b95792ff117c268dd4d1c096dc2e
sokpheng001 <kimchansokpheng123@gmail.com>
2025-03-07 15:54:28 +0700

    update: updated server

--- src/main/java/client/repository/UserRepository.java
+++ src/main/java/client/repository/UserRepository.java
@@ -30,27 +30,24 @@ public class UserRepository implements SokPhengRepository<User, Integer> {
     public Integer save(User user) {
         String sql = "INSERT INTO users (uuid, user_name, email, password, is_deleted, is_verified,created_at) " +
                 "VALUES (?, ?, ?, ?, ?, ?,?) RETURNING id";
-        try {
+        try(Connection connection  = GetDatabaseConnection.getConnection()) {
+            assert connection != null;
+            try(PreparedStatement stmt = connection.prepareStatement(sql)){
+                // Set parameters for the prepared statement
+                stmt.setString(1, user.getUuid());
+                stmt.setString(2, user.getUserName());
+                stmt.setString(3, user.getEmail());
+                stmt.setString(4, user.getPassword());
+                stmt.setBoolean(5, user.getIsDeleted());
+                stmt.setBoolean(6, user.getIsVerified());
+                stmt.setDate(7,Date.valueOf(LocalDate.now()));
 
-            try(Connection connection  = GetDatabaseConnection.getConnection()) {
-                assert connection != null;
-                try(PreparedStatement stmt = connection.prepareStatement(sql)){
-                    // Set parameters for the prepared statement
-                    stmt.setString(1, user.getUuid());
-                    stmt.setString(2, user.getUserName());
-                    stmt.setString(3, user.getEmail());
-                    stmt.setString(4, user.getPassword());
-                    stmt.setBoolean(5, user.getIsDeleted());
-                    stmt.setBoolean(6, user.getIsVerified());
-                    stmt.setDate(7,Date.valueOf(LocalDate.now()));
-
-                    // Execute query and get the auto-generated ID
-                    ResultSet rs = stmt.executeQuery();
-                    if (rs.next()) {
-                        user.setId(rs.getLong("id"));  // Set the generated id to the user object
-                    }
-                    return Math.toIntExact(user.getId());
+                // Execute query and get the auto-generated ID
+                ResultSet rs = stmt.executeQuery();
+                if (rs.next()) {
+                    user.setId(rs.getLong("id"));  // Set the generated id to the user object
                 }
+                return Math.toIntExact(user.getId());
             }
         } catch (Exception exception){
             System.out.println("[!] Error while saving user: " + exception.getMessage());
